3e886aab5e8a1898d856be24976999d0dd520041
mazzei <alessandro.mazzei@di.unito.it>
2018-03-21 17:55:43 +0100

    Added 1. exploration tree function 2. 1hot representation conversion for PoS and deps

--- main.py
+++ main.py
@@ -1,4 +1,83 @@
+import os
+import numpy
+from collections import OrderedDict
 from conllu import parse, parse_tree
+
+globalInteger = 1;
+
+
+#print numpy.eye(5, dtype=int)[4]
+
+
+
+def myprint(d):    
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            myprint(v)
+        else:
+            print "{0} : {1}".format(k, v)
+
+
+def myPrintNode(tn):
+    d = tn.data
+    print d.get("id"), d.get("form")
+
+def myVisit(l):
+    num = 0
+    print num
+    num = num + 1
+    for tn in l:
+        d = {tn.data.get("id") : tn.data}
+        myPrintNode(tn)
+        for c in tn.children:
+            d[c.data.get("id")] = c.data
+            myPrintNode(c)
+        od = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
+
+
+def exploreTree(tn):
+    global globalInteger
+    if(tn.children):
+        print globalInteger , "---------" , globalInteger
+        d = {"depRel" :  tn.data.get("deprel") , "uPoS" :  tn.data.get("upostag") , "groupId" : globalInteger, "originalPosition" : tn.data.get("id") , "form" : tn.data.get("form"), "isHead" : True}
+        l=[d]
+        for c in tn.children:
+            dc = {"depRel" :  c.data.get("deprel") , "uPoS" :  c.data.get("upostag") , "groupId" : globalInteger, "originalPosition" : c.data.get("id") , "form" : c.data.get("form"), "isHead" : False}
+            l.append(dc)
+        newL = sorted (l,key = lambda x : x['originalPosition'])
+        i = 1
+        for el in newL:
+            el["newPosition"]=i
+            i += 1
+        print newL
+        #print l
+        globalInteger = globalInteger + 1
+        for c in tn.children:            
+            exploreTree(c)
+
+
+#read the PoSs in the train file and convert into a hot-vector representation by using a dictionary
+f = os.popen('cut  -f 4 it-ud-dev.conll | sort | uniq')
+PoSList = filter(None,f.read().split("\n"))
+print PoSList #DEBUG
+PoS2HVRPoS = {}
+for PoS,i in zip (PoSList,range(len(PoSList)) ):
+    PoS2HVRPoS[PoS] = ",".join(map(str, numpy.eye(len(PoSList), dtype=int)[i]))
+print 'AUX=',PoS2HVRPoS['AUX'] #DEBUG
+
+#read the deps the train file and convert into a hot-vector representation by using a dictionary
+f = os.popen('cut  -f 8 it-ud-dev.conll | sort | uniq')
+depList = filter(None,f.read().split("\n"))
+print depList #DEBUG
+dep2HVRPoS = {}
+for dep,i in zip (depList,range(len(depList)) ):
+    dep2HVRPoS[dep] = ",".join(map(str, numpy.eye(len(depList), dtype=int)[i]))
+print 'xcomp=',dep2HVRPoS['xcomp'] #DEBUG
+
+
+    
+        
+
 data = """
 1   The     the    DET    DT   Definite=Def|PronType=Art   4   det     _   _
 2   quick   quick  ADJ    JJ   Degree=Pos                  4   amod    _   _
@@ -13,4 +92,12 @@ data = """
 
 """
 
-print parse_tree(data)
+pt = parse_tree(data)
+
+#print pt
+#print "----------\n"
+#myprint (pt[0][1][0][0])
+#myVisit(pt)
+exploreTree(pt[0])
+
+
